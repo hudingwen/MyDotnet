7002bff8c96617307d7a83a54a5c395919e63910
hudingwen <765472804@qq.com>
2024-06-16 13:53:28 +0800

    fix

--- MyDotnet/Tasks/QuartzJob/Job_Nightscout_Apple_Quartz.cs
+++ MyDotnet/Tasks/QuartzJob/Job_Nightscout_Apple_Quartz.cs
@@ -69,79 +69,88 @@ namespace MyDotnet.Tasks.QuartzJob
                     var pushWechatID = nsInfo.Find(t => t.code.Equals(NsInfo.pushWechatID)).content;
                     var pushCompanyCode = nsInfo.Find(t => t.code.Equals(NsInfo.pushCompanyCode)).content;
 
-                    //查询苹果
-                    var appleApiList = await _dicService.GetDicData(DicAppleInfo.AppleApiList);
-                    if(appleApiList != null && appleApiList.Count > 0)
+                    try
                     {
-                        var preInnerUser = await _dicService.GetDicDataOne(NsInfo.KEY, NsInfo.preInnerUser);
-                        var pushUsers = preInnerUser.content.Split(",", StringSplitOptions.RemoveEmptyEntries);
-                        foreach (var apple in appleApiList)
+                        //查询苹果
+                        var appleApiList = await _dicService.GetDicData(DicAppleInfo.AppleApiList);
+                        if (appleApiList != null && appleApiList.Count > 0)
                         {
-                            var token = AppleHelper.GetNewAppleToken(apple.code, apple.content, apple.content2);
-                            var list = await AppleHelper.GetDevices(token, 1, 200, "PROCESSING");
-
-                            //先查找谁审核通过了
-                            List<string> hasDevices = null;
-                            processingDevices.TryGetValue(apple.code, out hasDevices);
-                            if (hasDevices == null)
+                            var preInnerUser = await _dicService.GetDicDataOne(NsInfo.KEY, NsInfo.preInnerUser);
+                            var pushUsers = preInnerUser.content.Split(",", StringSplitOptions.RemoveEmptyEntries);
+                            foreach (var apple in appleApiList)
                             {
-                                processingDevices.Add(apple.code, new List<string>());
+                                var token = AppleHelper.GetNewAppleToken(apple.code, apple.content, apple.content2);
+                                var list = await AppleHelper.GetDevices(token, 1, 200, "PROCESSING");
+
+                                //先查找谁审核通过了
+                                List<string> hasDevices = null;
                                 processingDevices.TryGetValue(apple.code, out hasDevices);
-                            }
-                            var hasSendDevices = new List<string>();
-                            foreach (var udid in hasDevices)
-                            {
-                                var findUdid = list.data.Find(t => t.attributes.udid.Equals(udid));
-                                if(findUdid == null) {
-                                    //审核通知推送
-                                    if (pushUsers.Length > 0)
-                                    {
-                                        foreach (var userid in pushUsers)
-                                        {
-                                            var pushData = new WeChatCardMsgDataDto();
-                                            pushData.cardMsg = new WeChatCardMsgDetailDto();
-                                            pushData.cardMsg.keyword1 = $"设备审核完成:{udid}";
-                                            pushData.cardMsg.keyword2 = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-                                            pushData.cardMsg.url = frontPage;
-                                            pushData.cardMsg.template_id = pushTemplateID_Alert;
-                                            pushData.info = new WeChatUserInfo();
-                                            pushData.info.id = pushWechatID;
-                                            pushData.info.companyCode = pushCompanyCode;
-                                            pushData.info.userID = userid;
-                                            await _weChatConfigServices.PushCardMsg(pushData);
-                                        }
-                                    }
-                                    hasSendDevices.Add(udid);
-                                }
-                            }
-                            //去除审核完成的
-                            foreach (var udid in hasSendDevices)
-                            {
-                                hasDevices.Remove(udid);
-                            }
-
-                            //再加入审核中的设备
-                            foreach (var device in list.data)
-                            {
-                                var findAddDevice =  hasDevices.Find(t => t.Equals(device.attributes.udid));
-                                if (findAddDevice == null)
+                                if (hasDevices == null)
                                 {
-                                    hasDevices.Add(device.attributes.udid);
+                                    processingDevices.Add(apple.code, new List<string>());
+                                    processingDevices.TryGetValue(apple.code, out hasDevices);
+                                }
+                                var hasSendDevices = new List<string>();
+                                foreach (var udid in hasDevices)
+                                {
+                                    var findUdid = list.data.Find(t => t.attributes.udid.Equals(udid));
+                                    if (findUdid == null)
+                                    {
+                                        //审核通知推送
+                                        if (pushUsers.Length > 0)
+                                        {
+                                            foreach (var userid in pushUsers)
+                                            {
+                                                var pushData = new WeChatCardMsgDataDto();
+                                                pushData.cardMsg = new WeChatCardMsgDetailDto();
+                                                pushData.cardMsg.keyword1 = $"设备审核完成:{udid}";
+                                                pushData.cardMsg.keyword2 = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                                                pushData.cardMsg.url = frontPage;
+                                                pushData.cardMsg.template_id = pushTemplateID_Alert;
+                                                pushData.info = new WeChatUserInfo();
+                                                pushData.info.id = pushWechatID;
+                                                pushData.info.companyCode = pushCompanyCode;
+                                                pushData.info.userID = userid;
+                                                await _weChatConfigServices.PushCardMsg(pushData);
+                                            }
+                                        }
+                                        hasSendDevices.Add(udid);
+                                    }
+                                }
+                                //去除审核完成的
+                                foreach (var udid in hasSendDevices)
+                                {
+                                    hasDevices.Remove(udid);
+                                }
+
+                                //再加入审核中的设备
+                                foreach (var device in list.data)
+                                {
+                                    var findAddDevice = hasDevices.Find(t => t.Equals(device.attributes.udid));
+                                    if (findAddDevice == null)
+                                    {
+                                        hasDevices.Add(device.attributes.udid);
+                                    }
                                 }
                             }
-                        }
-                        
-                        
-                    }
 
-                    
-                    //查询配置
-                    var stepTimeDic = await _dicService.GetDicDataOne(DicAppleInfo.AppleApiConfig, DicAppleInfo.AppleApiConfig_stepTimes);
-                    var checkFinish = await _dicService.GetDicDataOne(DicAppleInfo.AppleApiConfig, DicAppleInfo.AppleApiConfig_checkIsExis);
-                    //间隔
-                    Thread.Sleep(stepTimeDic.content.ObjToInt() * 1000 * 60);
-                    //主动退出
-                    existStatus = checkFinish.content;
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        LogHelper.logApp.Error("苹果查询异常", ex);
+                    }
+                    finally
+                    { 
+                        //查询配置
+                        var stepTimeDic = await _dicService.GetDicDataOne(DicAppleInfo.AppleApiConfig, DicAppleInfo.AppleApiConfig_stepTimes);
+                        var checkFinish = await _dicService.GetDicDataOne(DicAppleInfo.AppleApiConfig, DicAppleInfo.AppleApiConfig_checkIsExis);
+                        //间隔
+                        Thread.Sleep(stepTimeDic.content.ObjToInt() * 1000 * 60);
+                        //主动退出
+                        existStatus = checkFinish.content;
+                    }
                 }
                 
             }
