c7c3aec0d6e3c1f92c4d112875d6b9045013f715
hudingwen <765472804@qq.com>
2024-06-23 23:19:40 +0800

    fix

--- MyDotnet/Domain/Dto/Ns/NsUploadBloodInfo.cs
+++ MyDotnet/Domain/Dto/Ns/NsUploadBloodInfo.cs
@@ -7,7 +7,12 @@
     {
         public double sgv { get; set; }
         public long date { get; set; }
-        public string direction { get; set; }
         public string type { get; set; } = "sgv";
+
+
+        public string direction { get; set; } = "Flat";
+        public string device { get; set; } = "aiyundiy";
+        public string dateString { get { return DateTimeOffset.FromUnixTimeMilliseconds(date).UtcDateTime.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"); }  } 
+
     }
 }
