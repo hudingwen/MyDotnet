cb90855d1ea9f27a7dc0da0e7c8acc4604942fd9
hudingwen <765472804@qq.com>
2026-03-09 13:02:51 +0800

    fix

---
+++ MyDotnet/Domain/Dto/Guiji/GuiQinyouLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyDotnet.Domain.Dto.Guiji
+{
+    public class GuiQinyouLoginDto
+    {
+        public string phone { get; set; }
+        public string agreementVersions { get; set; } = "{}";
+        public string password { get; set; }
+    }
+}
